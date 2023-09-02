4a7a09ab53275abc095e083ee7c1b191b102be05
jorgeabrahan <jorge24abrahan@gmail.com>
2023-09-02 01:53:36 -0600

    CHange Instructions to uppercase and change operation methods instruction name and ask for file path

--- Stack_Data_Structure/Stack_Data_Structure.cpp
+++ Stack_Data_Structure/Stack_Data_Structure.cpp
@@ -7,14 +7,23 @@ using namespace std;
 
 int main()
 {
+    string filePath;
+    cout << "Ingresa la dirección del archivo con el código fuente:"<<endl;
+    cout << "> ";
+    getline(cin, filePath);
+
+    ifstream InterpreterFile(filePath);
+    if (!InterpreterFile) {
+        cerr << "---- Archivo no encontrado ----" << endl;
+        return 1;
+    }
     string text; // variable to store the first line of the text file
-    ifstream InterpreterFile("Interpreter.txt"); // Create ifstream object from the txt file
     getline(InterpreterFile, text); // get the first line from the text file and save it in "text"
     InterpreterFile.close(); // close the file
 
     vector<string> elements;
     size_t pos = 0; // variable to store position of commas
-    string separator = ","; // variable that indicates the separator within elements in the text
+    string separator = " "; // variable that indicates the separator within elements in the text
     while ((pos = text.find(separator)) != string::npos) { // while there's still a comma in the text
         elements.push_back(text.substr(0, pos)); // add element to the vector
         text.erase(0, pos + separator.length()); // remove element from the text
@@ -25,40 +34,42 @@ int main()
     for (const auto& element : elements) {
         if (element == ">>") {
             float number;
-            cout << "Insert a number: " << endl;
+            cout << "Ingrese un valor:" << endl;
+            cout << "> ";
             // while user is not typing a valid number
             while (!(cin >> number)) {
-                cout << "ERROR. Please enter a valid number: " << endl; // ask user to input a number again
+                cout << "ERROR. Porfavor ingrese un numero valido:" << endl; // ask user to input a number again
+                cout << "> ";
                 cin.clear(); // clear error status from standard input to allow a new input
                 cin.ignore(numeric_limits<streamsize>::max(), '\n'); // clear invalid input from buffer
             }
             stack.push(number); // add number to stack
             continue;
         }
-        if (element == "<<") { cout << stack; continue; }
 
         // --------- Basic methods --------- //
-        if (element == "drop") { stack.drop(); continue; }
-        if (element == "top") { cout << "(top) returned: " << stack.top() << endl; continue; }
-        if (element == "pop") { cout << "(pop) returned: " << stack.pop() << endl; continue; }
+        if (element == "TOP") { cout << "(top) returned: " << stack.top() << endl; continue; }
+        if (element == "POP") { cout << "(pop) returned: " << stack.pop() << endl; continue; }
 
         // --------- Management methods --------- //
-        if (element == "swap") { stack.swap(); continue; }
-        if (element == "dup") { stack.dup(); continue; }
-        if (element == "depth") { stack.depth(); continue; }
-        if (element == "pick") { stack.pick(); continue; }
-        if (element == "dupn") { stack.dupn(); continue; }
-        if (element == "dropn") { stack.dropn(); continue; }
-        if (element == "rolld") { stack.rolld(); continue; }
-        if (element == "rollu") { stack.rollu(); continue; }
-        if (element == "clear") { stack.clear(); continue; }
+        if (element == "CLEAR") { stack.clear(); continue; }
+        if (element == "DEPTH") { stack.depth(); continue; }
+        if (element == "DROP") { stack.drop(); continue; }
+        if (element == "DROPN") { stack.dropn(); continue; }
+        if (element == "DUP") { stack.dup(); continue; }
+        if (element == "DUPN") { stack.dupn(); continue; }
+        if (element == "PICK") { stack.pick(); continue; }
+        if (element == "ROLLD") { stack.rolld(); continue; }
+        if (element == "ROLLU") { stack.rollu(); continue; }
+        if (element == "SHOW") { cout << stack; continue; }
+        if (element == "SWAP") { stack.swap(); continue; }
 
         // --------- Operation methods --------- //
-        if (element == "add") { stack.add(); continue; }
-        if (element == "sub") { stack.sub(); continue; }
-        if (element == "div") { stack.div(); continue; }
-        if (element == "mult") { stack.mult(); continue; }
-        if (element == "pot") { stack.pot(); continue; }
+        if (element == "+") { stack.add(); continue; }
+        if (element == "-") { stack.sub(); continue; }
+        if (element == "/") { stack.div(); continue; }
+        if (element == "*") { stack.mult(); continue; }
+        if (element == "^") { stack.pot(); continue; }
 
 
         try {
@@ -70,8 +81,7 @@ int main()
         catch (const invalid_argument&) {
             // the element is Not a number
         }
-        cout << "The string \"" << element << "\" is not a valid stack instruction!" << endl;
-
+        cout << "La instruccion \"" << element << "\" no es valida!" << endl;
     }
 
     return 0;
